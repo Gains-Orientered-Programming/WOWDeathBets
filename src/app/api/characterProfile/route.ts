a8f38fa38235d8007f2861183eecc83418500d88
Mikkel Bentsen <bentsen405@gmail.com>
2023-10-19 23:28:50 +0200

    dont slap me

--- src/app/api/characterProfile/route.ts
+++ src/app/api/characterProfile/route.ts
@@ -1,14 +1,18 @@
 import { getDataFromApi } from "src/services/oAuth";
 import { CharacterProfileSummary } from "src/types/blizzard/characterProfileSummary.t";
 
-export async function GET(request: Request) {
+export async function GET(
+  request: Request,
+  context: { params: { region: string; realm: string; characterName: string } }
+) {
   const params = {
-    "namespace": "profile-classic1x-eu",
+    "namespace": `profile-classic1x-${context.params.region}`,
     "locale": "eu_GB",
   };
+
   const data = await getDataFromApi<CharacterProfileSummary>(
-    "eu",
-    `/profile/wow/character/nekrosh/frostper`,
+    context.params.region,
+    `/profile/wow/character/${context.params.realm}/${context.params.characterName}}`,
     params
   );
 

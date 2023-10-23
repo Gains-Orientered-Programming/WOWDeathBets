624442df02c4a5b8efa4fac734f372588db05e69
Mikkel Bentsen <bentsen405@gmail.com>
2023-10-24 01:04:55 +0200

    itemPanel

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["wow.zamimg.com"],
+    domains: [
+      "wow.zamimg.com",
+      "static.bigbrain.gg",
+      "s3.us-east-2.amazonaws.com",
+    ],
   },
   experimental: {
     serverActions: true,
---
+++ src/app/characters/[region]/[realm]/[characterName]/(ItemPanel)/data.ts
@@ -0,0 +1,72 @@
+import { ItemPanelType } from "./types";
+
+export const charaterPanel: ItemPanelType = {
+  left: [
+    {
+      name: "HEAD",
+      slot: 1,
+    },
+    {
+      name: "NECK",
+      slot: 2,
+    },
+    {
+      name: "SHOULDER",
+      slot: 3,
+    },
+    {
+      name: "BACK",
+      slot: 15,
+    },
+    {
+      name: "CHEST",
+      slot: 5,
+    },
+    {
+      name: "SHIRT",
+      slot: 4,
+    },
+    {
+      name: "TABARD",
+      slot: 10,
+    },
+    {
+      name: "WRIST",
+      slot: 9,
+    },
+  ],
+  right: [
+    {
+      name: "HANDS",
+      slot: 10,
+    },
+    {
+      name: "WAIST",
+      slot: 6,
+    },
+    {
+      name: "LEGS",
+      slot: 7,
+    },
+    {
+      name: "FEET",
+      slot: 8,
+    },
+    {
+      name: "FINGER_1",
+      slot: 11,
+    },
+    {
+      name: "FINGER_2",
+      slot: 12,
+    },
+    {
+      name: "TRINKET_1",
+      slot: 13,
+    },
+    {
+      name: "TRINKET_2",
+      slot: 14,
+    },
+  ],
+};

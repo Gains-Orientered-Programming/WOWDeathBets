55a1f1aebc13aec31f089cbf93b41b97e6cbc332
Snoopsie1 <oliverrasoli@gmail.com>
2023-12-06 16:42:30 +0100

    extra test for failed login

--- src/test/features/login.feature
+++ src/test/features/login.feature
@@ -5,3 +5,9 @@ Feature: User Login
     When the user fills in their email and password
     And the user clicks on the login button
     Then the user should be successfully logged in
+
+  Scenario: Failed login
+    Given the user clicks on login
+    When the user fills in the wrong email and/or password
+    And the user clicks on the login button
+    Then the user should be shown an error message
--- src/test/step_definitions/login.steps.ts
+++ src/test/step_definitions/login.steps.ts
@@ -1,15 +1,33 @@
 import { Builder, By, until, WebDriver } from 'selenium-webdriver';
 import chrome from 'selenium-webdriver/chrome';
 import { loadFeature, defineFeature } from 'jest-cucumber';
+import axios, { AxiosError } from 'axios';
 
 const feature = loadFeature('src/test/features/login.feature');
 
 defineFeature(feature, (test) => {
 	let driver: WebDriver;
+	const testUsername = 'login-test-user';
+	const testEmail = 'login-test@test.dk';
+	const testPassword = 'login-test-pass';
 
 	beforeAll(async () => {
+		// Make test user
+		const response = await axios
+			.post(
+				'https://api-gateway-nyxm4.ondigitalocean.app/user-service/create-user',
+				{
+					username: testUsername,
+					email: testEmail,
+					password: testPassword,
+				}
+			)
+			.catch((err: AxiosError) => {
+				console.log(err);
+			});
+
 		const chromeOptions = new chrome.Options();
-		// chromeOptions.addArguments('--headless'); // Run Chrome in headless mode
+		chromeOptions.addArguments('--headless'); // Run Chrome in headless mode
 		// chromeOptions.addArguments('--disable-gpu'); // Disable GPU hardware acceleration
 		// chromeOptions.addArguments('--window-size=1920,1080'); // Specify window size
 
@@ -21,7 +39,56 @@ defineFeature(feature, (test) => {
 
 	afterAll(async () => {
 		await driver.quit();
+		// Delete test user
+		const response = await axios.delete(
+			`https://api-gateway-nyxm4.ondigitalocean.app/user-service/user/by-username/${testUsername}`
+		);
+		if (!response) console.error('User not found');
 	});
+
+	test('Failed login', ({ given, when, then }) => {
+		given('the user clicks on login', async () => {
+			await driver.get('http://localhost:3000');
+			const element = driver.findElement(
+				By.xpath('/html/body/main/div[1]/div[1]/div/div/ul/a/li')
+			);
+
+			await element.click();
+			await driver.wait(until.urlContains('login'));
+		});
+
+		when('the user fills in the wrong email and/or password', async () => {
+			const emailInput = driver.findElement(
+				By.xpath('/html/body/main/div[1]/div[2]/div/div/form/div[1]/div/input')
+			);
+			await driver.wait(until.elementIsVisible(emailInput));
+			await emailInput.sendKeys('prxy-post_man_DO@example.com');
+
+			const passwordInput = driver.findElement(
+				By.xpath('/html/body/main/div[1]/div[2]/div/div/form/div[2]/div/input')
+			);
+			await driver.wait(until.elementIsVisible(passwordInput));
+			await passwordInput.sendKeys('pot-proxy-PassDO');
+		});
+
+		when('the user clicks on the login button', async () => {
+			const loginButton = driver.findElement(
+				By.xpath('/html/body/main/div[1]/div[2]/div/div/form/div[3]/button')
+			);
+			await driver.wait(until.elementIsVisible(loginButton));
+			await loginButton.click();
+		});
+
+		then('the user should be shown an error message', async () => {
+			const errorElement = await driver.wait(
+				until.elementLocated(
+					By.xpath('/html/body/main/div[1]/div[2]/div/div/form/div[4]')
+				)
+			);
+			await driver.wait(until.elementIsVisible(errorElement));
+			expect(await errorElement.getText()).toBe('Wrong password or email');
+		});
+	}, 30000);
 
 	test('Successful login', ({ given, when, then }) => {
 		given('the user clicks on login', async () => {
@@ -39,13 +106,13 @@ defineFeature(feature, (test) => {
 				By.xpath('/html/body/main/div[1]/div[2]/div/div/form/div[1]/div/input')
 			);
 			await driver.wait(until.elementIsVisible(emailInput));
-			await emailInput.sendKeys('proxy-post_man_DO@example.com');
+			await emailInput.sendKeys(testEmail);
 
 			const passwordInput = driver.findElement(
 				By.xpath('/html/body/main/div[1]/div[2]/div/div/form/div[2]/div/input')
 			);
 			await driver.wait(until.elementIsVisible(passwordInput));
-			await passwordInput.sendKeys('post-proxy-PassDO');
+			await passwordInput.sendKeys(testPassword);
 		});
 
 		when('the user clicks on the login button', async () => {
@@ -63,7 +130,7 @@ defineFeature(feature, (test) => {
 			await driver.wait(until.elementLocated(element));
 			const username = await driver.findElement(element);
 			await driver.wait(until.elementIsVisible(username));
-			expect(await username.getText()).toBe('PROXY-digital-ocean-postman_per');
+			expect(await username.getText()).toBe(testUsername);
 
 			// Check if there is a JWT in the local storage
 			const jwt = await driver.executeScript(
@@ -71,5 +138,5 @@ defineFeature(feature, (test) => {
 			);
 			expect(jwt).not.toBeNull();
 		});
-	}, 20000);
+	}, 60000);
 });

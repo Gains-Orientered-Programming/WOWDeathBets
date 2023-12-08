53844b68f23384a432723fbb0c52820fab94d727
Mikkel Bentsen <bentsen405@gmail.com>
2023-12-08 13:51:42 +0100

    folder structure

---
+++ src/test/Betting/e2e/step_definitions/bet.steps.ts
@@ -0,0 +1,58 @@
+import { loadFeature, defineFeature } from "jest-cucumber";
+import { Builder, By, until, WebDriver } from "selenium-webdriver";
+import chrome from "selenium-webdriver/chrome";
+
+const feature = loadFeature("src/test/features/bettingform.feature");
+
+defineFeature(feature, (test) => {
+  let driver: WebDriver;
+
+  beforeAll(async () => {
+    const chromeOptions = new chrome.Options();
+    // chromeOptions.addArguments('--headless'); // Run Chrome in headless mode
+    // chromeOptions.addArguments('--disable-gpu'); // Disable GPU hardware acceleration
+    // chromeOptions.addArguments('--window-size=1920,1080'); // Specify window size
+
+    driver = await new Builder()
+      .forBrowser("chrome")
+      .setChromeOptions(chromeOptions)
+      .build();
+  });
+
+  afterAll(async () => {
+    await driver.quit();
+  });
+
+  test("Submitting the form on the bet page", ({ given, when, then }) => {
+    given("the user is on the betting page", async () => {
+      await driver.get(
+        "http://localhost:3000/characters/eu/nekrosh/petrice/bet"
+      );
+      await driver.wait(until.urlContains("characters"));
+    });
+
+    when(
+      "the user fill out the form and clicks on the button to create a bet",
+      async () => {
+        const input = await driver.findElement(
+          By.xpath("/html/body/main/div[1]/div[2]/div/div/form/div[4]/input")
+        );
+        const button = await driver.findElement(
+          By.xpath("/html/body/main/div[1]/div[2]/div/div/form/button")
+        );
+        await driver.wait(until.elementIsVisible(input));
+        await input.sendKeys("1000");
+        await driver.wait(until.elementIsVisible(button));
+        await button.click();
+      }
+    );
+
+    then("the user should create a bet", async () => {
+      const element = By.xpath("/html/body/div/ol/li/div/div[1]");
+      await driver.wait(until.elementLocated(element));
+      const toast = await driver.findElement(element);
+      await driver.wait(until.elementIsVisible(toast));
+      expect(await toast.isDisplayed()).toBe(true);
+    });
+  }, 20000); // Increase overall test timeout here
+});
---
+++ src/test/Betting/unit/BettingService.test.ts
@@ -0,0 +1,143 @@
+import {
+  getAllBettings,
+  getBettingsByUserId,
+  getHighestBettings,
+  createBetting,
+} from "../../api/betting-services";
+import axios from "axios";
+
+jest.mock("axios");
+
+describe("Betting Service Tests", () => {
+  const mockedAxios = axios as jest.Mocked<typeof axios>;
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("should create a betting", async () => {
+    const mockBettingData = {
+      userId: "123",
+      characterName: "Petrice",
+      region: "eu",
+      realm: "nekrosh",
+      amount: 1000,
+    };
+    const mockResponse = {
+      data: {
+        userId: "123",
+        characterName: "Petrice",
+        region: "eu",
+        realm: "nekrosh",
+        amount: 1000,
+      },
+    };
+
+    mockedAxios.post.mockResolvedValueOnce(mockResponse);
+
+    const result = await createBetting(mockBettingData);
+
+    expect(mockedAxios.post).toHaveBeenCalledWith(
+      "https://api-gateway-nyxm4.ondigitalocean.app/betting-service/bettings",
+      mockBettingData
+    );
+    expect(result).toEqual(mockResponse.data);
+  });
+
+  it("should get bettings by user ID", async () => {
+    const mockUserId = "123";
+    const mockResponse = {
+      data: [
+        {
+          _id: "6093eb433fc0a31f2cc1cc83", // Mocked ID
+          userId: "test",
+          characterName: "test_characterName",
+          region: "test_region",
+          realm: "test_realm",
+          amount: 0,
+        },
+        {
+          _id: "7093eb433fc0a31f2cc1cc84", // Mocked ID
+          userId: "test",
+          characterName: "test_characterName2",
+          region: "test_region",
+          realm: "test_realm",
+          amount: 0,
+        },
+      ],
+    };
+
+    mockedAxios.get.mockResolvedValueOnce(mockResponse);
+
+    const result = await getBettingsByUserId(mockUserId);
+
+    expect(mockedAxios.get).toHaveBeenCalledWith(
+      `https://api-gateway-nyxm4.ondigitalocean.app/betting-service/bettings/userId/${mockUserId}`
+    );
+    expect(result).toEqual(mockResponse.data);
+  });
+
+  it("should get all bettings", async () => {
+    const mockResponse = {
+      data: [
+        {
+          _id: "6093eb433fc0a31f2cc1cc83", // Mocked ID
+          userId: "test",
+          characterName: "test_characterName",
+          region: "test_region",
+          realm: "test_realm",
+          amount: 0,
+        },
+        {
+          _id: "7093eb433fc0a31f2cc1cc84", // Mocked ID
+          userId: "test",
+          characterName: "test_characterName2",
+          region: "test_region",
+          realm: "test_realm",
+          amount: 0,
+        },
+      ],
+    };
+
+    mockedAxios.get.mockResolvedValueOnce(mockResponse);
+
+    const result = await getAllBettings();
+
+    expect(mockedAxios.get).toHaveBeenCalledWith(
+      "https://api-gateway-nyxm4.ondigitalocean.app/betting-service/bettings"
+    );
+    expect(result).toEqual(mockResponse.data);
+  });
+
+  it("should get highest bettings", async () => {
+    const mockResponse = {
+      data: [
+        {
+          _id: "6093eb433fc0a31f2cc1cc83", // Mocked ID
+          userId: "test",
+          characterName: "test_characterName",
+          region: "test_region",
+          realm: "test_realm",
+          amount: 0,
+        },
+        {
+          _id: "7093eb433fc0a31f2cc1cc84", // Mocked ID
+          userId: "test",
+          characterName: "test_characterName2",
+          region: "test_region",
+          realm: "test_realm",
+          amount: 0,
+        },
+      ],
+    };
+
+    mockedAxios.get.mockResolvedValueOnce(mockResponse);
+
+    const result = await getHighestBettings();
+
+    expect(mockedAxios.get).toHaveBeenCalledWith(
+      "https://api-gateway-nyxm4.ondigitalocean.app/betting-service/bettings/most"
+    );
+    expect(result).toEqual(mockResponse.data);
+  });
+});

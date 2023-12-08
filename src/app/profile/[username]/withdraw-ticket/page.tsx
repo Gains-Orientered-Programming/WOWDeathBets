877594a8cf8d8e2c8d6db0b4a96ed1b54ed662d5
Snoopsie1 <oliverrasoli@gmail.com>
2023-12-08 17:05:21 +0100

    build go bonk

--- src/app/profile/[username]/withdraw-ticket/page.tsx
+++ src/app/profile/[username]/withdraw-ticket/page.tsx
@@ -1,11 +1,12 @@
-import TicketForm from "./Form";
+import TicketForm from './Form';
 
 const WithdrawPage = () => {
-  return (
-    <div className="w-full">
-      <TicketForm />
-    </div>
-  );
+	return (
+		<div className="w-full">
+			<TicketForm username={''} />{' '}
+			{/* lavede en tom username prop fordi ellers breaker build */}
+		</div>
+	);
 };
 
 export default WithdrawPage;

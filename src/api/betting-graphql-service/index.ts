ae2d673b7afdb99610dee94c967dd9663ad06d9b
Mikkel Bentsen <bentsen405@gmail.com>
2023-12-10 15:19:26 +0100

    removed lagcy folder that ruined pipeline

--- src/api/betting-graphql-service/index.ts
+++
@@ -1,9 +0,0 @@
-import { Betting } from "@/src/types/betting-service.t";
-import { request, gql } from "graphql-request";
-
-const endpoint = "http://localhost:8080/api";
-
-export const getBettings = gql`
-query Query {
-
-}`;

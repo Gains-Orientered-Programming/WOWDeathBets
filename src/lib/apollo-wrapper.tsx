eb6210773434d2028b89bd3b34beb61512593f64
Mikkel Bentsen <bentsen405@gmail.com>
2023-12-10 19:28:58 +0100

    graphql route changed to new digital coean url

--- src/lib/apollo-wrapper.tsx
+++ src/lib/apollo-wrapper.tsx
@@ -10,12 +10,12 @@ import {
 
 const GRAPHQL_ENDPOINT =
   process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT ||
-  "https://api-gateway-nyxm4.ondigitalocean.app/betting-service/api";
+  "https://api-gateway-nyxm4.ondigitalocean.app/betting-service-gql/api";
 
 function makeClient() {
   const httpLink = new HttpLink({
     // this needs to be an absolute url, as relative urls cannot be used in SSR
-    uri: "https://api-gateway-nyxm4.ondigitalocean.app/betting-service/api",
+    uri: "https://api-gateway-nyxm4.ondigitalocean.app/betting-service-gql/api",
     // you can disable result caching here if you want to
     // (this does not work if you are rendering your page with `export const dynamic = "force-static"`)
     fetchOptions: { cache: "no-store" },
--- src/lib/client.ts
+++ src/lib/client.ts
@@ -3,14 +3,14 @@ import { registerApolloClient } from "@apollo/experimental-nextjs-app-support/rs
 
 const GRAPHQL_ENDPOINT =
   process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT ||
-  "https://api-gateway-nyxm4.ondigitalocean.app/betting-service/api";
+  "https://api-gateway-nyxm4.ondigitalocean.app/betting-service-gql/api";
 
 export const { getClient } = registerApolloClient(() => {
   return new ApolloClient({
     cache: new InMemoryCache(),
     link: new HttpLink({
       // this needs to be an absolute url, as relative urls cannot be used in SSR
-      uri: "https://api-gateway-nyxm4.ondigitalocean.app/betting-service/api",
+      uri: "https://api-gateway-nyxm4.ondigitalocean.app/betting-service-gql/api",
       // you can disable result caching here if you want to
       // (this does not work if you are rendering your page with `export const dynamic = "force-static"`)
       // fetchOptions: { cache: "no-store" },

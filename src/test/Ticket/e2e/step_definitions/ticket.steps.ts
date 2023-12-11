641220c8aa5e8664dcb7ca779afec51f9e820882
Mikkel Bentsen <bentsen405@gmail.com>
2023-12-11 02:26:05 +0100

    ticket e2e now headless

--- src/test/Ticket/e2e/step_definitions/ticket.steps.ts
+++ src/test/Ticket/e2e/step_definitions/ticket.steps.ts
@@ -1,29 +1,25 @@
-import { deleteSeededUsers, seedUsers } from '../../../utils/seedUsers';
-import { User } from '../../../../types/user';
+import { deleteSeededUsers, seedUsers } from "../../../utils/seedUsers";
+import { User } from "../../../../types/user";
 import { loadFeature, defineFeature } from "jest-cucumber";
 import { Builder, By, until, WebDriver } from "selenium-webdriver";
 import chrome from "selenium-webdriver/chrome";
 
-const feature = loadFeature(
-  "src/test/Ticket/e2e/features/ticketform.feature"
-);
+const feature = loadFeature("src/test/Ticket/e2e/features/ticketform.feature");
 
 defineFeature(feature, (test) => {
   let driver: WebDriver;
   let testUser: User;
 
-
   beforeAll(async () => {
     testUser = await seedUsers();
 
-		// Check if testUser is properly seeded
-		if (!testUser.email || !testUser.password) {
-			throw new Error('Failed to seed test user');
-		}
-
+    // Check if testUser is properly seeded
+    if (!testUser.email || !testUser.password) {
+      throw new Error("Failed to seed test user");
+    }
 
     const chromeOptions = new chrome.Options();
-   // chromeOptions.addArguments("--headless"); // Run Chrome in headless mode
+    chromeOptions.addArguments("--headless"); // Run Chrome in headless mode
     // chromeOptions.addArguments('--disable-gpu'); // Disable GPU hardware acceleration
     // chromeOptions.addArguments('--window-size=1920,1080'); // Specify window size
 
@@ -39,20 +35,28 @@ defineFeature(feature, (test) => {
     await deleteSeededUsers(testUser);
   });
 
-  test("Submitting either a depsoit or withdraw form on the profile page", ({ given, when, then }) => {
+  test("Submitting either a depsoit or withdraw form on the profile page", ({
+    given,
+    when,
+    then,
+  }) => {
     given("the user is on the profile page", async () => {
-      await driver.get(
-        "http://localhost:3000/profile/testUser/deposit-ticket"
-      );
+      await driver.get("http://localhost:3000/profile/testUser/deposit-ticket");
       await driver.wait(until.urlContains("testUser"));
     });
 
     when(
       "the user fill out the form and clicks on the button to create a deposit or withdraw ticket",
       async () => {
-        const inputName = await driver.findElement(By.css("[data-testid='characterNameInput']"));
-        const inputAmount = await driver.findElement(By.css("[data-testid='amountInput']"));
-        const button = await driver.findElement(By.css("[data-testid='button']"));
+        const inputName = await driver.findElement(
+          By.css("[data-testid='characterNameInput']")
+        );
+        const inputAmount = await driver.findElement(
+          By.css("[data-testid='amountInput']")
+        );
+        const button = await driver.findElement(
+          By.css("[data-testid='button']")
+        );
         await driver.wait(until.elementIsVisible(inputName));
         await inputName.sendKeys("Petrice");
         await driver.wait(until.elementIsVisible(inputAmount));
